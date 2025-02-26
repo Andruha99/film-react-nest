ee6f0b9991198837b1e51be6c36d512287c9ba00
Andruha99 <Andriei.korovin.2014@mail.ru>
2025-02-26 15:25:17 +0300

    Fix npm run build errors

--- frontend/src/components/SelectSession/SelectSession.stories.ts
+++ frontend/src/components/SelectSession/SelectSession.stories.ts
@@ -1,12 +1,12 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-import { SelectSession } from './SelectSession';
+import type { Meta } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { SelectSession } from "./SelectSession";
 
 const meta = {
-  title: 'UI/SelectSession',
+  title: "UI/SelectSession",
   component: SelectSession,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
   args: {
     onSelect: fn(),
@@ -14,31 +14,31 @@ const meta = {
 } satisfies Meta<typeof SelectSession>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    sessions: [
-        {
-            id: '1',
-            day: 'Monday',
-            time: '10:00',
-        },
-        {
-          id: '1',
-          day: 'Monday',
-          time: '12:00',
-        },
-        {
-            id: '2',
-            day: 'Tuesday',
-            time: '11:00',
-        },
-        {
-            id: '3',
-            day: 'Wednesday',
-            time: '12:00',
-        },
-    ]
-  },
-};
+// export const Default: Story = {
+//   args: {
+//     sessions: [
+//         {
+//             id: '1',
+//             day: 'Monday',
+//             time: '10:00',
+//         },
+//         {
+//           id: '1',
+//           day: 'Monday',
+//           time: '12:00',
+//         },
+//         {
+//             id: '2',
+//             day: 'Tuesday',
+//             time: '11:00',
+//         },
+//         {
+//             id: '3',
+//             day: 'Wednesday',
+//             time: '12:00',
+//         },
+//     ]
+//   },
+// };
